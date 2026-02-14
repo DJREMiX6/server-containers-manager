4037269c87b747b17ef23c768208f4d8e68e40a2
Francesco Girelli <francescogirellilime@gmail.com>
2026-02-14 19:22:29 +0100

    DockerIntegration - Added Serilog

--- backend/ServerContainerManager.API/Program.cs
+++ backend/ServerContainerManager.API/Program.cs
@@ -1,27 +1,55 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-builder.AddServiceDefaults();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+        .Configuration(
+            new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json").Build())
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try 
 {
-    app.MapOpenApi();
+    Log.Information("Bootstrapping");
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddServiceDefaults();
+
+    // Serilog Setup
+    builder.Logging.ClearProviders();
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services);
+    });
+
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    app.MapDefaultEndpoints();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    Log.Information("Running");
+    await app.RunAsync();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
