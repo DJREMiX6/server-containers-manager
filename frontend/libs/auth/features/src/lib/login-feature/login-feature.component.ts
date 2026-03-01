558d513b101942460dc9291068a38d1ea4aee893
Francesco Girelli <francescogirellilime@gmail.com>
2026-03-01 21:32:25 +0100

    Initial Login Page UI setup

--- frontend/libs/auth/features/src/lib/login-feature/login-feature.component.ts
+++ frontend/libs/auth/features/src/lib/login-feature/login-feature.component.ts
@@ -1,14 +1,42 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { IconFieldModule } from "primeng/iconfield";
-import { InputIconModule } from "primeng/inputicon";
-import { InputTextModule } from "primeng/inputtext";
-import { ButtonModule } from "primeng/button";
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { form, FormField, required, submit } from '@angular/forms/signals';
+import { LoginFormModel } from '../models/login-form-model';
 
 @Component({
   selector: 'lib-login-feature.component',
-  imports: [IconFieldModule, InputIconModule, InputTextModule, ButtonModule],
+  imports: [
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    ButtonModule,
+    MessageModule,
+    FloatLabelModule,
+    FormField,
+  ],
   templateUrl: './login-feature.component.html',
   styleUrl: './login-feature.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginFeatureComponent {}
+export class LoginFeatureComponent {
+  private loginModel = signal<LoginFormModel>({
+    username: '',
+    password: '',
+  });
+
+  protected loginForm = form(this.loginModel, (schema) => {
+    required(schema.username, { message: 'Username is required' });
+    required(schema.password, { message: 'Password is required' });
+  });
+
+  protected loginBtnClicked_evt() {
+    submit(this.loginForm, async (form) => {
+      console.log('test');
+    });
+  }
+}
