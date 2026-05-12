836e1007009f777201e409bc3ac18f84bcd430c2
Francesco Girelli <francescogirellilime@gmail.com>
2026-05-13 01:34:50 +0200

    Fixed error with UsersController and Query handler

--- backend/ServerContainerManager.Application/Queries/User/CheckUsernameAvailability/CheckUsernameAvailabilityQueryHandler.cs
+++ backend/ServerContainerManager.Application/Queries/User/CheckUsernameAvailability/CheckUsernameAvailabilityQueryHandler.cs
@@ -2,14 +2,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using ServerContainerManager.Application.Commands.Abstraction;
+using ServerContainerManager.Application.Queries.Abstraction;
 using ServerContainerManager.Domain.Entities.Auth;
 
 namespace ServerContainerManager.Application.Queries.User.CheckUsernameAvailability
 {
     internal sealed class CheckUsernameAvailabilityQueryHandler(
         ILogger<CheckUsernameAvailabilityQueryHandler> logger,
-        UserManager<AppUser> userManager) : ICommandHandler<CheckUsernameAvailabilityQuery, CheckUsernameAvailabilityQueryResult>
+        UserManager<AppUser> userManager) : IQueryHandler<CheckUsernameAvailabilityQuery, CheckUsernameAvailabilityQueryResult>
     {
         private readonly ILogger<CheckUsernameAvailabilityQueryHandler> _logger = logger;
         private readonly UserManager<AppUser> _userManager = userManager;
