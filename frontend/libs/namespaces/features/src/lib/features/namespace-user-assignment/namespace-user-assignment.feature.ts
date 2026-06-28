c75e1fdb07a0d0283a1015a5cca042b05e24be37
Francesco Girelli <francescogirellilime@gmail.com>
2026-06-28 21:36:06 +0200

    Implemented modal control and namespace list reload. Correctly implemented mocked picklist v1.0

--- frontend/libs/namespaces/features/src/lib/features/namespace-user-assignment/namespace-user-assignment.feature.ts
+++ frontend/libs/namespaces/features/src/lib/features/namespace-user-assignment/namespace-user-assignment.feature.ts
@@ -1,9 +1,65 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { PickListModule } from 'primeng/picklist';
+import { Button } from 'primeng/button';
+
+type User = { id: string; username: string };
 
 @Component({
   selector: 'lib-namespace-user-assignment',
-  imports: [],
+  imports: [PickListModule, Button],
   templateUrl: './namespace-user-assignment.feature.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NamespaceUserAssignmentFeature {}
+export class NamespaceUserAssignmentFeature {
+  public readonly selectedNamespaceId = input.required<undefined | string>();
+
+  public readonly operationCanceled = output<void>();
+  public readonly operationCompleted = output<void>();
+
+  protected readonly sourceUsers = signal<User[]>([
+    {
+      id: '1',
+      username: 'testUser1',
+    },
+    {
+      id: '2',
+      username: 'testUser2',
+    },
+    {
+      id: '3',
+      username: 'testUser3',
+    },
+    {
+      id: '4',
+      username: 'testUser4',
+    },
+    {
+      id: '5',
+      username: 'testUser5',
+    },
+  ]);
+  protected readonly targetUsers = signal<User[]>([]);
+
+  protected shouldShowSourceFilter() {
+    return this.sourceUsers().length > 3;
+  }
+
+  protected shouldShowTargetFilter() {
+    return this.targetUsers().length > 3;
+  }
+
+  protected onConfirmBtnClick(): void {
+    this.operationCompleted.emit();
+  }
+
+  protected onCancelBtnClick(): void {
+    this.operationCanceled.emit();
+  }
+}
