b4b85c6353d383b4cb8eb9d5496afaf70c62e2da
Francesco Girelli <francescogirellilime@gmail.com>
2026-09-07 21:10:23 +0200

    Fixed minor bug with ContainerLabel ZOD schema that expected value to be non empty but it can actually happen

--- frontend/libs/containers/data/src/lib/models/dtos/container-label.dto.schema.ts
+++ frontend/libs/containers/data/src/lib/models/dtos/container-label.dto.schema.ts
@@ -2,6 +2,6 @@ import * as z from "zod";
 import { ContainerLabelDto } from "./container-label.dto";
 
 export const ContainerLabelDtoSchema: z.ZodType<ContainerLabelDto> = z.object({
-    key: z.string().nonempty(),
-    value: z.string().nonempty()
+  key: z.string().nonempty(),
+  value: z.string().nonoptional(),
 });
